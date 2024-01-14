05366a96a4fd3ec17d4d1011ac18a438d10867ef
paul <bares.paul@gmail.com>
2024-01-14 15:55:22 +0400

    doc

--- src/main/java/com/digital/wallet/service/DigitalWalletService.java
+++ src/main/java/com/digital/wallet/service/DigitalWalletService.java
@@ -7,20 +7,35 @@ import org.springframework.data.domain.Pageable;
 
 public interface DigitalWalletService {
     /**
-     * Executes a deposit for a customer in the digital wallet.
+     * Executes a deposit for a customer in the digital wallet. The amount should be between
+     * {@link com.digital.wallet.config.Constants#MINIMUM_DEPOSIT} and
+     * {@link com.digital.wallet.config.Constants#MAXIMUM_DEPOSIT}.
      *
      * @param customerId the unique identifier of the customer
      * @param currency   the currency of the deposit. As per ISO 4217.
      *                   <a href="https://en.wikipedia.org/wiki/ISO_4217">https://en.wikipedia.org/wiki/ISO_4217</a>
      * @param amount     the amount to be deposited
-     *                   // Throwing exception if > 10 000  or < 10
      */
     void executeDeposit(Long customerId, String currency, BigDecimal amount, String remark);
 
     /**
-     * Max £5,000
+     * Executes a withdrawal for a customer in the digital wallet. The amount should be between 0
+     * and {@link com.digital.wallet.config.Constants#MAXIMUM_WITHDRAWAL}.
+     *
+     * @param customerId the unique identifier of the customer
+     * @param currency   the currency of the withdrawal. As per ISO 4217.
+     *                   <a href="https://en.wikipedia.org/wiki/ISO_4217">https://en.wikipedia.org/wiki/ISO_4217</a>
+     * @param amount     the amount to be withdrawn
+     * @param remark     a remark or description for the withdrawal
      */
     void executeWithdrawal(Long customerId, String currency, BigDecimal amount, String remark);
 
+    /**
+     * Retrieves the transactions for a customer.
+     *
+     * @param customerId the unique identifier of the customer
+     * @param pageable   the pagination information
+     * @return a Page object containing the transactions
+     */
     Page<WalletTransaction> getTransactions(Long customerId, Pageable pageable);
 }
